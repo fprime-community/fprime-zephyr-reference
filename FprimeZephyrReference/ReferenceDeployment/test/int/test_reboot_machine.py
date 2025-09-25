f81f6fa2cb6e30bdf4da020b3f9387f0568c2720
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-09-24 17:14:42 -0700

    added script to reboot machine

---
+++ FprimeZephyrReference/ReferenceDeployment/test/int/test_reboot_machine.py
@@ -0,0 +1,7 @@
+
+
+def test_send_command(fprime_test_api):
+    """Send a reboot command to reboot the system.
+    """
+    fprime_test_api.send_and_assert_command(fprime_test_api.get_mnemonic('Svc.FatalHandler') + '.' + 'REBOOT', max_delay=0.500)
+
